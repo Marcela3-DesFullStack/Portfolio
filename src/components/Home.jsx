b5e8278b9cccea742f1cb2b14fa0af2dc64d44cc
Marcela3-DesFullStack <claudiamarcela.nc14@gmail.com>
2024-02-06 09:42:42 +0100

    Reviewing images path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
   return (
     
-    <div className="sm:h-[80vh] w-full h-[70vh] overflow-hidden bg-cover bg-center bg-no-repeat flex flex-col sm:flex-row items-center justify-around bg-customCobalt sm:h-[50vh]">
+    <div className="sm:h-[80vh] w-full h-[90vh] sm:h-[70vh] overflow-hidden bg-cover bg-center bg-no-repeat flex flex-col sm:flex-row items-center justify-around bg-customCobalt sm:h-[50vh]">
         <img src={photoMarce} className="sm:w-1/2 sm:transform sm:-translate-x-1/2 sm:top-[20%] absolute left-[50%] top-[175px] rounded-full" />   
         <div className="sm:top-[60%] sm:pl-10 sm:pr-[5%] sm:flex sm:flex-col sm:items-center sm:justify-center absolute left-[10%] top-[45%] text-customWhite">
             <p className="text-[25px] sm:text-[15px] ">Hi! I`m</p> 
